8096e0c3beacd67377cef4bf20325cb69605d750
JiSoo-Hwang <jisoo0507@naver.com>
2024-04-30 19:19:12 +0900

    commit

---
+++ GBangE/src/main/java/com/kh/common/EncodingFilter.java
@@ -0,0 +1,60 @@
+package com.kh.common;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+
+/**
+ * Servlet Filter implementation class EncodingFilter
+ */
+@WebFilter("/*")
+public class EncodingFilter extends HttpFilter implements Filter {
+       
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+     * @see HttpFilter#HttpFilter()
+     */
+    public EncodingFilter() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		//필터 : 기존 요청 흐름을 가로채서 특정 작업 후 해당 요청 흐름을 막아주거나 다시 이어주거나 처리하는 영역
+		
+		//해당 필터에서는 인코딩처리(UTF-8)을 진행하고 기존 흐름을 유지시킬 것
+		request.setCharacterEncoding("UTF-8");
+		
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ GBangE/src/main/java/com/kh/feed/model/vo/Feed.java
@@ -0,0 +1,5 @@
+package com.kh.feed.model.vo;
+
+public class Feed {
+
+}
