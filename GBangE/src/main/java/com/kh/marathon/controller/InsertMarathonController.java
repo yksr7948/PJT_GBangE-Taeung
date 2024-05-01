85db2327d02a30f82599d409d0fe11711ef1dfc7
Hyuwon12 <hyuwon12@gmail.com>
2024-05-01 14:47:42 +0900

    한번에 올리는 파일이 많습니다. 혹시라도 오류가 생길수 있으니, 자세히 체크해주세요
    올려야하는파일들
    1.500x400이라는 이름으로 시작하는 jpg파일들
    2.파일 경로가 /webapp/views/marathon 내부에 있는 css확장자 파일들(자바스크립트 실행에필요한 부트스트랩
    파일입니다.)
    3.index.jsp 변경점. 메뉴바 include
    4.com/kh/marathon 폴더 내부에 변경된 파일들
    marathonController,marathonDao,marathonservice
    5.마라톤 mapper.xml파일
    
    그외에 체크가 필요한사항
    프로젝트의 contextroot가 jsp로 잡혀있습니다 수정이 필요해보입니다 (수정하지않았음)

--- GBangE/src/main/java/com/kh/marathon/controller/InsertMarathonController.java
+++ GBangE/src/main/java/com/kh/marathon/controller/InsertMarathonController.java
@@ -1,16 +1,19 @@
 package com.kh.marathon.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.kh.marathon.model.service.MarathonService;
+
 /**
  * Servlet implementation class InsertMarathonController
  */
-@WebServlet("/InsertMarathonController")
+@WebServlet("/insert.ma")
 public class InsertMarathonController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -26,8 +29,21 @@ public class InsertMarathonController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//요청들어오면 테이블내의 데이터를 비우고 insert 관리자만 실행시킬수있게 처리
+		int result = 0;
+		result = new MarathonService().deleteAllMarathon();
+		if(result<0) {
+			request.getSession().setAttribute("msg", "초기화 실패");
+			response.sendRedirect(request.getContextPath());
+		}else {
+			result = new MarathonService().insertMarthon();
+			if(result>0) {
+				request.getSession().setAttribute("msg", "초기화 성공");				
+			}else {
+				request.getSession().setAttribute("msg", "초기화 실패");
+			}
+			response.sendRedirect(request.getContextPath());
+		}
 	}
 
 	/**
---
+++ GBangE/src/main/webapp/views/marathon/js/main.js
@@ -0,0 +1,46 @@
+(function ($) {
+    function Carousel1__onTranslated() {
+        $(".packages-carousel").trigger('play.owl.autoplay');
+        
+        $(".packages-carousel").attr('data-packages-carousel-autoplay-status', 'Y');
+    }
+    "use strict";
+    $(".packages-carousel").owlCarousel({
+        autoplay: true,
+        autoplayTimeout:2000,        
+        center: false,
+        dots: false,
+        loop: true,
+        margin: 25,
+        nav : true,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsiveClass: true,
+        responsive: {
+            items:3,
+            autoplayHoverPause:false,
+            onTranslated: Carousel1__onTranslated,
+        }
+    });
+ 
+    $('#carouselplaybtn').on('click',function(){
+        $(".packages-carousel").trigger('play.owl.autoplay');
+    
+        $(".packages-carousel").attr('data-packages-carousel-autoplay-status', 'Y');
+        alert("스와이프가 실행되었습니다.");
+        $("#carouselstopbtn").css("display","");
+        $("#carouselplaybtn").css("display","none");
+    });
+
+    $('#carouselstopbtn').on('click',function(){
+        $(".packages-carousel").trigger('stop.owl.autoplay');
+    
+        $(".packages-carousel").attr('data-packages-carousel-autoplay-status', 'N');
+        alert("스와이프가 중지되었습니다.");
+        $("#carouselplaybtn").css("display","");
+        $("#carouselstopbtn").css("display","none");
+    });
+
+})(jQuery);
